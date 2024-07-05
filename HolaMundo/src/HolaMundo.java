04ba72c57ada1df24c5111751f996b2450cdd8ef
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-07-05 12:42:15 -0500

    feat: :tada: Started the course of Java

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    /*clase principal con un metodo o funcion que es la puerta de entrada a la aplicacion, es el arranque, cualquier
+    clase que contenga este metodo principal se ejecuta como aplicacion autonoma*/
+    // Todo en Java es una clase y objeto menos los tipos primitivos los cuales representan un valor
+    // La JVM se encarga de ejecutar esta clase principal, y por eso tiene que ser un metodo publico
+    public static void main(String[] args){
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Repo para almacenar lecciones, ejercicios y demas sobre el curso completo de java, a futuro se implementaran las lecciones de Spring Boot
+#
+### - JDRO
