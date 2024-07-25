0c2f90197cba7af955942c361a707aead8b6a86e
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-07-25 17:53:20 -0500

    fear: :sparkles: finished nineth section System Class

--- ClaseSystem/src/EjecutarProgramaDelSO.java
+++ ClaseSystem/src/EjecutarProgramaDelSO.java
@@ -6,10 +6,25 @@ public class EjecutarProgramaDelSO {
 
         Process proceso; // Cuando se ejecuta un programa como el editor de texto, lo que runtime retornara será un objeto del tipo process
 
-        if(System.getProperty("os.name").startsWith("Linux")) { // Evaluamos el sistema operativo, y si es linux lo ejecute
-            proceso = rt.exec("notepad"); // dentro de proceso guardamos lo que retorne la ejecucion, que es algo de tipo process, por eso creamos proceso de la clase Process
-            // Con exec ejecutamos el programa que se le pase como argumento, y tambien requiere de manejo de errores, nos lo pide asi que toca ponerlo
+        try {
+
+            if(System.getProperty("os.name").startsWith("Linux")) { // Evaluamos el sistema operativo, y si es linux, que lo ejecute
+                proceso = rt.exec("gedit"); // dentro de proceso guardamos lo que retorne la ejecucion, que es algo de tipo process, por eso creamos proceso de la clase Process
+                // Con exec ejecutamos el programa que se le pase como argumento, y tambien requiere de manejo de errores, nos lo pide asi que toca ponerlo
+                // con exec tambien podemos ejecutar comandos en la terminal
+            } else {
+                proceso = rt.exec("notepad");
+            }
+            proceso.waitFor(); // Este metodo lo que hace es esperar hasta que cerremos o finalice el programa o el proceso que hayamos abierto para continuar con el resto de codigo
+            // este metodo tambien equiere de un manejo de excepciones
+
+        } catch (Exception e) {
+
+            System.err.println("El comando es desconocido" + e.getMessage());
+            System.exit(1);
+
         }
+        System.out.println("Se ha cerrado el editor");
 
     }
 
